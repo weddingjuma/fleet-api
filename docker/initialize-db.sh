82edd9a45c9a568055cc6bb7787eedf5b9af4e37
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-04-06 11:28:20 +0200

    cluster-init --services data,index,query

--- docker/initialize-db.sh
+++ docker/initialize-db.sh
@@ -11,6 +11,6 @@ set -e
 
 [[ -z "${PROJECT}" ]] && die "You must pass a project name in parameter. For example: $0 fleet"
 
-docker-compose -p ${PROJECT} exec couchbaseserver couchbase-cli cluster-init --cluster-username=admin --cluster-password=123456 --cluster-ramsize=256
+docker-compose -p ${PROJECT} exec couchbaseserver couchbase-cli cluster-init --services data,index,query --cluster-username=admin --cluster-password=123456 --cluster-ramsize=256
 docker-compose -p ${PROJECT} exec couchbaseserver couchbase-cli bucket-create --bucket fleet-prod --bucket-type couchbase -c localhost --bucket-ramsize 256 -u admin -p 123456
 docker-compose -p ${PROJECT} exec couchbaseserver couchbase-cli user-manage --set -c localhost -u admin -p 123456 --rbac-username fleet-prod --rbac-password 123456 --roles bucket_admin[fleet-prod] --auth-domain local
