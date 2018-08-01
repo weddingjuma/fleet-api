1b9cf0d68ee84563aa79da8c7d60218aa34e7f5f
Julien Boyer <julien.boyer@mapotempo.com>
2018-08-01 12:10:40 +0200

    Fix Routing errors for company controller

--- app/controllers/api/v01/admin/companies_controller.rb
+++ app/controllers/api/v01/admin/companies_controller.rb
@@ -15,59 +15,61 @@
 # along with Mapotempo. If not, see:
 # <http://www.gnu.org/licenses/agpl.html>
 #
-module Api::V01::Admin
-  class CompaniesController < ApiController
-    before_action :authenticate_admin
-    skip_before_action :authenticate
+module Api::V01
+  module Admin
+    class CompaniesController < ApiController
+      before_action :authenticate_admin
+      skip_before_action :authenticate
 
-    def index
-      skip_authorization
-      render json: Company.all.to_a,
-             each_serializer: CompanySerializer
-    end
-
-    def show
-      skip_authorization
-      company = nil
-      if params[:user_id]
-        user = User.find_by(params[:user_id])
-        company = user&.company
-      elsif params[:id]
-        company = Company.find(params[:id])
+      def index
+        skip_authorization
+        render json: Company.all.to_a,
+              each_serializer: CompanySerializer
       end
 
-      if company
-        render json: company,
-               serializer: CompanySerializer
-      else
-        render body: nil, status: :not_found
+      def show
+        skip_authorization
+        company = nil
+        if params[:user_id]
+          user = User.find_by(params[:user_id])
+          company = user&.company
+        elsif params[:id]
+          company = Company.find(params[:id])
+        end
+
+        if company
+          render json: company,
+                serializer: CompanySerializer
+        else
+          render body: nil, status: :not_found
+        end
       end
-    end
 
-    def create
-      skip_authorization
-      company = Company.new
-      company.assign_attributes(company_params)
+      def create
+        skip_authorization
+        company = Company.new
+        company.assign_attributes(company_params)
 
-      if company.save
-        company.set_workflow
-        admin_user = company.create_admin_user(params[:user_email]) if params[:user_email]
+        if company.save
+          company.set_workflow
+          admin_user = company.create_admin_user(params[:user_email]) if params[:user_email]
 
-        render json: company,
-               serializer: CompanySerializer,
-               with_admin: admin_user
-      else
-        render json: company.errors, status: :unprocessable_entity
+          render json: company,
+                serializer: CompanySerializer,
+                with_admin: admin_user
+        else
+          render json: company.errors, status: :unprocessable_entity
+        end
       end
+
+      private
+
+      def company_params
+        params.permit(
+          :name
+        )
+      end
+
     end
-
-    private
-
-    def company_params
-      params.permit(
-        :name
-      )
-    end
-
   end
 end
